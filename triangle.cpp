954efc643b77e214b529231d983647c5a8a13792
Reward Edoyugbo <Raj@Rewards-MacBook-Pro.local>
2022-10-28 17:52:05 +0200

    resolved method 2

--- triangle.cpp
+++ triangle.cpp
@@ -33,6 +33,8 @@ float findTrianglePerimeter(float side1, float side2, float side3) {
 */
 float findTriangleArea(float base, float height) {
     //TODO STUDENT A
+    float sum = base * height;
+    return sum;
 }
 
 /**********************************************
