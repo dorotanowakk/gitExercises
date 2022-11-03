280469386e2f26a3b359fd09fc5f76e5be18c68b
James Anikwe <jay@Jamess-MacBook-Pro.local>
2022-11-03 18:45:40 +0100

    modified area method

--- triangle.cpp
+++ triangle.cpp
@@ -44,6 +44,9 @@ float findTriangleArea(float base, float height) {
 */
 float findTriangleArea(float side1, float side2, float side3) {
     //TODO STUDENT B
+    float s = ((side1+side2+side3)/2);
+    float area = sqrt(s*(s-side1)*(s-side2)*(s-side3));
+    return area;
 }
 
 /**********************************************
