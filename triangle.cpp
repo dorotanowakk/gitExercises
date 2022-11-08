d9f944a58eee933b1211c230518784caf0831b46
Norest Chikuse <chikusenono@yahoo.com>
2022-11-08 16:33:49 +0100

    Commented out some methods

--- triangle.cpp
+++ triangle.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 /**********************************************
@@ -9,9 +10,10 @@ using namespace std;
 */
 float findThirdAngle(float ang1, float ang2) {
     //TODO STUDENT A
-    
-}
+    float angle3 = 180 - (ang1 + ang2);
 
+    return angle3;
+}
 /**********************************************
 * Method to calculate perimeter of triangle
 * it returns sum of all sides
@@ -19,9 +21,10 @@ float findThirdAngle(float ang1, float ang2) {
 * you can check if each of sides measure is smaller then sum of rests (additional step)
 * all rules: side1 < side2 + side3, side2 < side 1 + side3, side 3 < side 1 + side2
 */
+
 float findTrianglePerimeter(float side1, float side2, float side3) {
     //TODO STUDENT B
-    
+ /*   
     if(side1 < side2 + side3 and side2 < side1 + side3 and side3 < side1 + side2)
     {
         float perim = side1 + side2 + side3;
@@ -34,25 +37,35 @@ float findTrianglePerimeter(float side1, float side2, float side3) {
         }
     
 }
+        cout >> ("Perimeter is: perim");
+        }
+    else
+        {
+        cout >> ("Error sides rule broken");
+        }
+        return perim;
+    }
+*/
 
+float findTriangleArea(float base, float height) {
+   float AreaSum = 0.5 * base * height;
+   return AreaSum; 
+}
 /**********************************************
 * Method to calculate area of triangle
 * it returns in: a (base) * h (height)
 */
-float findTriangleArea(float base, float height) {
-    //TODO STUDENT A
-}
-
 /**********************************************
 * Method to calculate area of triangle
 * you have to check and use Heron's formula
 * (you need to use cmath library)
 */
+
 float findTriangleArea(float side1, float side2, float side3) {
     //TODO STUDENT B
     float area, s;
-    s = (a +b + c) /2
-    area = sqr(s*(s-side1) (s-side2) (s-side3));
+    s = (side1 + side2 + side3) /2
+    area = sqr(s*(s-side1* s-side2 * s-side3));
     return area;
 }
 
@@ -71,7 +84,7 @@ void findThirdAngleUI() {
 */
 void findTriangleAreaUI(float h, float b) {
     //TODO STUDENT B
-    area = b /2 * h;
+    area = (b /2) * h;
     return area;
 }
 
@@ -91,17 +104,15 @@ int main()
 {
     //TODO STUDENT A and STUDENT B
     //check written UI methods in second part of exercise
-    
-    float s1, s2, s3, area, perim;
-    s1 = 25;
-    s2 = 50;
-    s3 = 70; 
-
-    //Evaluate findTrianglePerimeter
+       
+/*
+    //Evaluate findTrianglePerimeter - Student B
+    float s1 = 25, s2 = 50, s3 70, area, perim;
     perim = findTrianglePerimeter(s1,s2,s3);
     cout << ("Perimeter of Triangle: " << perim);
-
-    //Evaluate findTriangleAreaUI
+*/
+    //Evaluate findTriangleAreaUI - Student B;
+    float t_base, t_height, area;
     cout << ("Enter triangle base > ");
     cin >> t_base;
     cout << ("Enter triangle height");
@@ -109,8 +120,18 @@ int main()
     area = findTriangleAreaUI(t_base, t_height);
     cout << ("Triangle Area is: " << area);
 
-    //Evaluate findTriangleArea
+    //Evaluate findTriangleArea Heron's formular - Student B
+    float side1, side2, side3, area;
     area = findTriangleArea(side1, side2,side3);
     cout << ("Area of Triangle is: " << area);
+
+    //Evaluate findTriangleArea - Student A
+    float base = 20;
+    float height = 30;
+
+    float TriangleArea = findTriangleArea(base, height);
+     
+    std::cout<< "Area of triangle =" << TriangleArea << std::endl;
+
     return 0;
 }
