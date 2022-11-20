d07403f9cd8f672cfd89499cf4115d0b54ff4045
Clifford-02 <mawoniclifford02@gmail.com>
2022-11-20 15:14:57 +0100

    studB code

--- studB2.cpp
+++ studB2.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+int main(){
+    float side1;
+    float side2;
+    float side3;
+    float perimeter;
+
+    std::cout << "Please enter side1:\n> ";
+    std::cin >> side1;
+
+    std::cout << "Please enter side2:\n> ";
+    std::cin >> side2;
+
+    std::cout << "Please enter side3:\n> ";
+    std::cin >> side3;
+
+    perimeter = side1+side2+side3;
+
+    std::cout << "The perimeter is equal to: " << perimeter;
+
+
+return 0;
+}
